555e3b263250aef8b1252d11b02650ed75578092
novastick <99231858+novastick@users.noreply.github.com>
2022-11-03 11:51:05 +1100

    shooting

--- gamescences/player/current_score.gd
+++ gamescences/player/current_score.gd
@@ -9,6 +9,8 @@ func _ready():
 
 
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- global.gd
+++ global.gd
@@ -3,3 +3,6 @@ extends Node
 
 var current_score = 0
 var ammo = 0
+
+func _ready():
+	pass
